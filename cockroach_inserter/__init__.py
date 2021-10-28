b73fca6a4470b9bf3e2f98425e466e4142697500
Daniel Endraws <daniel.endraws@berkely.edu>
2021-10-27 21:34:29 -0700

    modified code and handled more cases

--- cockroach_inserter/__init__.py
+++ cockroach_inserter/__init__.py
@@ -6,9 +6,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    data = req.params.get('data')
-    try:
-        insterter.write_into_db(data)
-        return func.HttpResponse("Payload inserted into cockroach sucessfully!", status_code = 200)
-    except BaseException as e:
-        return func.HttpResponse(str(e), status_code = 400)
+    data = req.get_json()
+    if not data:
+        return func.HttpResponse("No input!", status_code = 400)
+    return insterter.write_into_db(data)
--- cockroach_inserter/insterter.py
+++ cockroach_inserter/insterter.py
@@ -1,16 +1,23 @@
 import psycopg2
 import pathlib
+import azure.functions as func
+import logging
 
 
-def write_into_db(payload):
-    link = "postgresql://ofir:zPfIA64Mol4tYZCf@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/wifi_crowdsourcing?sslmode=verify-full&sslrootcert=" + pathlib.Path(__file__).parent + "/root.crt&options=--cluster%3Dwifi-crowdsourcing-4313"
+def write_into_db(payload: dict):
+    link = "postgresql://ofir:zPfIA64Mol4tYZCf@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/wifi_crowdsourcing?sslmode=verify-full&sslrootcert=" + str(pathlib.Path(__file__).parent) + "/root.crt&options=--cluster%3Dwifi-crowdsourcing-4313"
     conn = psycopg2.connect(link)
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO wifi_data (latitude, longitude, floor_id, building_id, date_time, "
-                    "wifi_name, download_speed, upload_speed, outage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                    vars=(payload["lat"], payload["long"], payload['flrID'], payload['bldID'], payload["dateTime"], payload["wifiName"],
-                    payload["dwnldSpd"], payload["upldSpd"], payload["outage"]))
-
+        try:
+            cur.execute("INSERT INTO wifi_data (latitude, longitude, floor_id, building_id, date_time, "
+                        "wifi_name, download_speed, upload_speed, outage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        vars=(payload["lat"], payload["long"], payload['flrID'], payload['bldID'], payload["dateTime"], payload["wifiName"],
+                        payload["dwnldSpd"], payload["upldSpd"], payload["outage"]))
+            logging.info("Inserted into DB")
+        except BaseException as err:
+            logging.exception(str(err))
+            return func.HttpResponse("Malformed JSON input", status_code = 400)
     conn.commit()
     conn.close()
+    return func.HttpResponse("Payload inserted into cockroach sucessfully!", status_code = 200)
 
